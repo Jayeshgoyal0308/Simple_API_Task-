5904acc6d7de1295146378466561dad353084f7e
Jayesh Goyal <jayeshgoyal28@gmail.com>
2022-08-17 23:46:12 +0530

    backend done

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express')
-
+const https = require('https')
+const axios = require('axios')
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.get("/" , (req,res)=>{
+
+    const url = "https://jsonplaceholder.typicode.com/users";
+
+  axios.get(url).then(resp => {
+    res.send(resp.data)
+});  
+
+})
+
 app.listen(PORT , () => {
     console.log(`Server listening on PORT ${PORT}...`);
 })
+
